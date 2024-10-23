ec79e9b2de0191383d6741471667e4e03192a037
Erik Bauer <RKspeedfreak@web.de>
2024-10-23 15:37:16 +0200

    Projektdateien hinzufügen.

---
+++ WourHourTracking/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+
+namespace WourHourTracking
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public DateTime CurrentTime;
+        public const string FilePath = @"\TimeTable\TimeTable.csv";
+        public List<TimeEntry> TimeEntries;
+        public TimeEntry CurrentEntry;
+        public string absolutePath;
+
+        public MainWindow()
+        {
+            absolutePath = Directory.GetCurrentDirectory() + FilePath;
+            CurrentEntry = new TimeEntry();
+            CurrentTime = DateTime.Now;
+            CheckFileExists(absolutePath);
+            TimeEntries = new List<TimeEntry>(TimeEntry.GetTimeEntries(absolutePath));
+            InitializeComponent();
+            header.Content = $"Hallo, Heute ist {CurrentTime.DayOfWeek} der: " +
+                $"{CurrentTime.Date.ToShortDateString()}. Es ist {CurrentTime.ToShortTimeString()}";
+        }
+
+        private void CheckFileExists(string absolutePath)
+        {
+            string directoryPath = Path.GetDirectoryName(absolutePath);
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            if (!File.Exists(absolutePath))
+            {
+                MessageBox.Show("File nicht vorhanden");
+                File.CreateText(absolutePath);
+            }
+        }
+
+        private void CreateNewTimeEntry(object sender, RoutedEventArgs e)
+        {
+            if (CurrentEntry.StartTime != TimeEntries[TimeEntries.Count - 1].StartTime)
+            {
+                if (CurrentEntry.EntryID == 0)
+                {
+                    CurrentEntry.EntryID = TimeEntries.Count + 1;
+                    CurrentEntry.StartTime = CurrentTime;
+                    createEntryButton.Content = "tracking läuft...";
+                }
+                else
+                {
+                    CurrentEntry.EndTime = DateTime.Now;
+                    CurrentEntry.Notes = dayNotes.Text;
+                    createEntryButton.Content = "Eintrag fertig";
+                    WriteEntry(absolutePath);
+                    TimeEntries.Add(CurrentEntry);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Eintrag wurde bereits erstellt");
+            }
+        }
+
+        private void AlterEntry(object sender, RoutedEventArgs e)
+        {
+        }
+
+        public void WriteEntry(string path)
+        {
+            using (StreamWriter WriteFile = new StreamWriter(path, true))
+            {
+                try
+                {
+                    if (CurrentEntry != null)
+                    {
+                        WriteFile.WriteLine(CurrentEntry.FullEntryToString());
+                    }
+                    else { throw new Exception(); }
+                }
+                catch (NullReferenceException)
+                {
+                    MessageBox.Show("Etwas ist schief gelaufen. Versuchs nochmal");
+                }
+            }
+        }
+    }
+}
---
+++ WourHourTracking/Model/TimeEntry.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WourHourTracking
+{
+    public class TimeEntry
+    {
+        public int EntryID;
+        public DateTime StartTime;
+        public DateTime EndTime;
+        public string Notes;
+
+        public TimeEntry()
+        {
+        }
+
+        public TimeEntry(string entry)
+        {
+            string[] data = entry.Split(',');
+            EntryID = int.Parse(data[0]);
+            StartTime = DateTime.Parse(data[1]);
+            EndTime = DateTime.Parse(data[2]);
+            Notes = data[3];
+        }
+
+        public TimeEntry(int id, DateTime start, DateTime end, string notes)
+        {
+            EntryID = id;
+            StartTime = start;
+            EndTime = end;
+            Notes = notes;
+        }
+
+        public string FullEntryToString()
+        {
+            return $"{EntryID}, {StartTime.ToString()}, {EndTime}, {Notes}";
+        }
+
+        public static List<TimeEntry> GetTimeEntries(string sourcePath)
+        {
+            List<TimeEntry> result = new List<TimeEntry>();
+
+            using (StreamReader sr = new StreamReader(sourcePath)) 
+            {
+               
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        TimeEntry entry = new TimeEntry(line);
+                        result.Add(entry);
+                    }
+                
+            }
+
+            return result ;
+        }
+    }
+}
